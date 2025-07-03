5083a0512ff86c55d106d7f339deae7214210727
nandinikamaraj <nandinikamaraj17@gmail.com>
2025-07-04 00:28:04 +0530

    Fix: added port fallback for Render deploy

--- index.js
+++ index.js
@@ -14,11 +14,16 @@ app.use(express.json());
 app.use('/api/tasks', taskRoutes);
 app.use('/api/users', userRoutes);
 
+// Fixed PORT fallback for Render
+const PORT = process.env.PORT || 3000;
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
+    app.listen(PORT, () =>
+      console.log(`Server running on port ${PORT}`)
     );
   })
   .catch((err) => console.error(err));
+
+  
